bc566d4b3d00f64beaff0419b2819f67de138b9d
Ian Lum <ianlum314@gmail.com>
2025-04-29 22:27:43 -0400

    add docstrings

--- python/visualizations.py
+++ python/visualizations.py
@@ -1,20 +1,45 @@
+"""
+Contains a class for visualizing the results of the genetic programming model.
+"""
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from matplotlib.patches import Patch
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from parse_tree import *
+from parse_tree import ParseTree
 from genetic_programming import IrisGP
 
 
 class Visualizations:
+    """
+    Class for visualizing the results of the genetic programming model.
+
+    Attributes:
+        parse_tree (ParseTree): A parse tree to evaluate, expected to be the
+            best parse tree from the GP model.
+        test_df (pd.DataFrame): The test set of the data.
+        test_results (pd.DataFrame): A DataFrame containing the results of the
+            predictions on the test set.
+    """
+
     def __init__(self, parse_tree: ParseTree, test_df: pd.DataFrame):
         self.parse_tree: ParseTree = parse_tree
         self.test_df: pd.DataFrame = test_df
         self.test_results: pd.DataFrame = self.tabulate_results()
 
-    def tabulate_results(self):
+    def tabulate_results(self) -> pd.DataFrame:
+        """
+        Generate a table of the performance of a parse tree on the test set.
+        The following columns are added:
+            Correct (bool): Whether the prediction was correct.
+            PredictedValue (int): The output value of the parse tree.
+            PredictedSpecies (str): The predicted species of the flower.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the results.
+        """
         test_results = self.test_df.copy()
         test_results["Correct"] = None
         test_results["PredictedValue"] = None
@@ -33,6 +58,15 @@ class Visualizations:
         return test_results
 
     def plot_predictions_by_dimension(self, part: str):
+        """
+        Plot the predictions of the parse tree by either the sepal or petal
+        dimensions. Points are colored by the predicted species. Correct
+        predictions are smaller and transparent, while incorrect predictions
+        are larger and opaque.
+
+        Args:
+            part (str): Either "Sepal" or "Petal".
+        """
         COLOR_MAP = {
             "Iris-setosa": "tab:blue",
             "Iris-versicolor": "tab:orange",
@@ -61,8 +95,6 @@ class Visualizations:
                 row[f"{part}LengthCm"],
                 row[f"{part}WidthCm"],
                 color=COLOR_MAP[row["PredictedSpecies"]],
-                # edgecolors=COLOR_MAP[row["Species"]],
-                # linewidths=2,
                 alpha=CORRECT_OPACITY if row["Correct"] else 1,
                 s=CORRECT_SIZE if row["Correct"] else INCORRECT_SIZE,
             )
@@ -82,6 +114,10 @@ class Visualizations:
         plt.show()
 
     def plot_confusion_matrix(self):
+        """
+        Plot the confusion matrix of the predicted species vs. the actual
+        species.
+        """
         cm = confusion_matrix(
             self.test_results["Species"], self.test_results["PredictedSpecies"]
         )
